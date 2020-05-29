f3e07677caa90a98aa7517e456055674d30be33c
xinchen <xzha9245@uni.sydney.edu.au>
2020-05-30 01:27:46 +1000

    dasdas

--- server.c
+++ server.c
@@ -697,7 +697,6 @@ int retrieve_file(connection_data_t *arg) {
             uint8_t *file_content = malloc(len);
             fread(file_content, 1, len, fd);
 
-            
             uint64_t num_of_bit = 0;
             uint64_t num_of_bytes = 1;
             uint8_t *compressed_msg = malloc(1);
@@ -709,7 +708,7 @@ int retrieve_file(connection_data_t *arg) {
             num_of_bytes += 1;
             res->msg.header = 0x70;
             set_bit(&res->msg.header, 4);
-            res->msg.p_length = bswap_64(num_of_bytes+20);
+            res->msg.p_length = bswap_64(num_of_bytes);
             write(d->socketfd, &res->msg, sizeof(res->msg.header)+sizeof(res->msg.p_length));
 
             padding = (8-(num_of_bit)%8) % 8;
