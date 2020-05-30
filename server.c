d832b36a2a080e79780ab93861171e22b2af91a1
xinchen <xzha9245@uni.sydney.edu.au>
2020-05-30 11:41:42 +1000

    asdasd

--- server.c
+++ server.c
@@ -722,6 +722,7 @@ int retrieve_file(connection_data_t *arg) {
         pthread_mutex_unlock(&s->lock);
         return 1;
     } 
+    
     pthread_mutex_unlock(&s->lock);
 
     //Add to the session list
