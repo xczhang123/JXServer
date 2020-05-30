0e4833819a24f0033b70e69c5c7605c5ae0f7a14
xinchen <xzha9245@uni.sydney.edu.au>
2020-05-30 11:30:48 +1000

    asdasd

--- session.c
+++ session.c
@@ -48,7 +48,6 @@ void session_array_delete(session_t *s, uint32_t id, uint64_t start, uint64_t le
         if (seg->id == id && seg->start == start && 
             seg->len == len && strcmp(seg->filename, filename) == 0) {
                 s->sessions[i] = s->sessions[--s->size];
-                // puts("DELETED");
                 break;
         }
     }
@@ -65,17 +64,18 @@ session_segment_t* session_array_get(session_t *s, int index) {
 }
 
 bool session_array_is_in(session_t *s, uint32_t id, uint64_t start, uint64_t len, char *filename) {
+    bool found = false;
     pthread_mutex_lock(&s->lock);
     for (int i = 0; i < s->size; i++) {
         session_segment_t *seg = session_array_get(s, i);
         if (seg->id == id && seg->start == start && 
             seg->len == len && strcmp(seg->filename, filename) == 0) {
-                return true;
+                found = true;
         }
     }
     pthread_mutex_unlock(&s->lock);
 
-    return false;
+    return found;
 }
 
 
