997e3bd9ca7cce15ccec002ab5a84187a5a07d11
xinchen <xzha9245@uni.sydney.edu.au>
2020-05-30 10:47:03 +1000

    dadas

--- session.c
+++ session.c
@@ -47,7 +47,7 @@ void session_array_delete(session_t *s, uint32_t id, uint64_t start, uint64_t le
         session_segment_t *seg = session_array_get(s, i);
         if (seg->id == id && seg->start == start && 
             seg->len == len && strcmp(seg->filename, filename) == 0) {
-                s->sessions[i] = s->sessions[s->size--];
+                s->sessions[i] = s->sessions[--s->size];
                 break;
         }
     }
